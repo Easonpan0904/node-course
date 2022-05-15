df7f83c708c1b07c597e9b42f74886f23e75a3f7
Eason <a28165965@gmail.com>
2022-05-15 14:08:53 +0800

    add try catch判斷到readfile-await.js

--- readfile/readfile-await.js
+++ readfile/readfile-await.js
@@ -3,7 +3,10 @@ const fs = require('fs/promises');
 // 會回傳給你一個 promise 物件，很像 readfile-promise2 裡的 getReadfilePromise
 
 let readFileAsync = (async function () {
-  let fsPromise = await fs.readFile('test.txt', 'utf-8');
-
-  console.log(fsPromise);
+  try {
+    let fsPromise = await fs.readFile('test.txt', 'utf-8');
+    console.log(fsPromise);
+  } catch (e) {
+    console.log(e);
+  }
 })();
