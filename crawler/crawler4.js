97ba19644c2f2b2daa5ab3338cd2097ca92c26de
Eason <a28165965@gmail.com>
2022-05-21 13:09:33 +0800

    reupload crawler4.js

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -10,7 +10,7 @@ const mysql = require('mysql2/promise');
     host: 'localhost',
     port: 3306,
     user: 'admin',
-    password: '123456',
+    password: '',
     database: 'eason_test_db',
   });
   let [data, fields] = await connection.execute('SELECT * FROM stocks');
